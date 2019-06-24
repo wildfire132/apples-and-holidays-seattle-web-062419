3d6303a4120be3976c60aaa4276abe7298102146
Katana Tran <tkatyk21@gmail.com>
2019-06-24 03:26:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,8 +47,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash.each do |season, holiday|
-    season.values
+  holiday_hash[:winter].values
+    
 
 end
 
