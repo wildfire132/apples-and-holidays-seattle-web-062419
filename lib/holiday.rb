72611b862179ed9796cd3aadfc39bba50cae2884
Katana Tran <tkatyk21@gmail.com>
2019-06-24 03:29:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,9 +60,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
-    puts "#{season}:
-    #{holiday}"
     season.each do |holiday, supply|
+      puts "#{season}:
+    #{holiday}: #{supply}"
   end
 end 
 end
