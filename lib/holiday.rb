f2265e3a54ec9734d3cd78f591a36981e46fd9b5
Katana Tran <tkatyk21@gmail.com>
2019-06-24 06:02:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,8 +73,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   
   holiday_hash.collect do |season, holiday|
-    holiday_hash.collect do |holiday, 
-  if holiday_hash[:season][:holiday].include?("BBQ")
+    holiday_hash.collect do |holiday, supply|
+  if supply.include?("BBQ")
       holiday 
   end
 end.flatten
