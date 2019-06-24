25d90788eaee37dd8b36595ea03fd22bdaa7eece
Katana Tran <tkatyk21@gmail.com>
2019-06-24 03:19:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holidays = holiday_hash.values 
-  holidays << "BBQ"
+  holiday_hash.each do |season, holiday|
+    if 
 
 end
 
